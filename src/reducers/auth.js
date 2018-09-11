46ca67d40914316b0442f75d110b6c220cbcd311
8p4hxc2 <8p4hxc2@gmail.com>
2018-09-11 11:08:28 +0200

    initial reducer commit

---
+++ src/reducers/auth.js
@@ -0,0 +1,28 @@
+// action types
+export const types = {
+    LOGIN_REQUEST: 'auth/LOGIN_REQUEST',
+    LOGIN_SUCCESS: 'auth/LOGIN_SUCCESS',
+    LOGIN_ERROR: 'auth/LOGIN_ERROR'
+}
+
+// action creators
+export const actions = {
+    loginRequest: (email, password) => ({ type: types.LOGIN_REQUEST, email, password })
+};
+
+// initial state
+const initialState = {
+
+};
+
+// reducers
+export default (state = initialState, actions) => {
+    switch (actions.type) {
+        case types.LOGIN_SUCCESS:
+            return { ...state, user: actions.user + Math.random() };
+
+        case types.LOGIN_ERROR:
+            return state;
+    }
+    return state;
+};
