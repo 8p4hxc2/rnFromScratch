6943f6980849ae83245726ff4eaadfe6c61cd581
8p4hxc2 <8p4hxc2@gmail.com>
2018-09-14 16:16:50 +0200

    connect redux to login screen and add a dispatch on button

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -8,16 +8,22 @@
 
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { actions as authActions } from '../../reducers/auth';
 import styles from './style';
 
 type Props = {};
-export default class App extends Component<Props> {
+class Login extends Component<Props> {
     render() {
+        const { auth, dispatch } = this.props;
+
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>LOGIN AAEEEEAAAA!</Text>
-                <Button title='Log me IN !' onPress={() => { }}></Button>
+                <Button title={auth.user} onPress={() => { dispatch(authActions.loginRequest()); }}></Button>
             </View>
         );
     }
 }
+
+export default connect((state) => (state))(Login);
