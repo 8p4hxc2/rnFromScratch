be7f6182a3dcd50389a6f916644aac00b33ed1cb
8p4hxc2 <8p4hxc2@gmail.com>
2018-09-14 16:17:08 +0200

    rename home screen

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -18,7 +18,7 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class Home extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
